73d381130566338d11f8de64345e9811e65e63b0
$.m.lnk <smjvlab@gmail.com>
2018-01-12 22:51:44 +0200

    This class shows the work of the while

--- src/pkgwhile/While.java
+++ src/pkgwhile/While.java
@@ -8,6 +8,25 @@ public class While {
             System.out.println("input: " + i);
             i--;
         }
-    }
+        
+        // Целевая часть цикла может быть пустой
+        int c=100;
+        int d=200;
+        
+        // рассчитать среднее значение переменных i и j
+        while (c++<d--){
+            /*у этого цикла отсутствует тело*/
+        System.out.println(c);
+        }
+        
+        int a = 10;
+        int b = 20;
+        while (a > b) {
+            System.out.println("Этa строка выводиться не будет ");
+            break;
+        }
+        
+        
 
+    }
 }
